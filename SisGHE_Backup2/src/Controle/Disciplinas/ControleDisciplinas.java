9ff01a286100fe308e8f1b2ca44d8e5641e4b83b
David <david@david-Aspire-4745.(none)>
2013-03-02 18:40:10 -0300

    refatoração das disciplinas recuperadas do cadastro do aluno, e setadas nas checkbox

--- SisGHE_Backup2/src/Controle/Disciplinas/ControleDisciplinas.java
+++ SisGHE_Backup2/src/Controle/Disciplinas/ControleDisciplinas.java
@@ -4,21 +4,22 @@
  */
 package Controle.Disciplinas;
 
-import DAO.Disciplinas.DeserializeSubject;
+import DAO.Disciplinas.DeserializaDisciplinas;
 import DAO.Disciplinas.SerializeSubject;
 import Modelo.Disciplinas.Disciplina;
 import Modelo.Disciplinas.Turma;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ArrayList;
+import Business.*;
 
 /**
  *
  * @author Axs
  */
-public class ControlSubject {
+public class ControleDisciplinas {
     
-    public ControlSubject() {}
+    public ControleDisciplinas() {}
     
     public static Disciplina createSubject (String name, String id, String pre_id, ArrayList <Turma> classes) {
         Disciplina subject = new Disciplina(name, id, pre_id, classes);
@@ -38,8 +39,12 @@ public class ControlSubject {
         SerializeSubject.serializingDisciplines(disciplines);
     }
     
-    public ArrayList <Disciplina> callDeserialize() {
-        ArrayList <Disciplina> disciplines = DeserializeSubject.deserializingDisciplines();
-        return disciplines;
+    public ArrayList <Disciplina> deserializaDisciplinas() {
+        ArrayList <Disciplina> disciplinas = DeserializaDisciplinas.deserializaDisciplinasXml();
+        return disciplinas;
+    }
+    
+    public static void chamaSelecionaCheckBox(){
+        SelecionaCheckBox.setaDisciplina();
     }
 }
